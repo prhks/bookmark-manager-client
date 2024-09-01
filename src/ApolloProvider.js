30084d02da56d9229d67c39842cecc1e3d282834
Prakash Kumar Singh <prakashaug98@gmail.com>
2024-09-01 23:06:53 +0530

    add proxy and url

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -9,7 +9,7 @@ import {
 import { setContext } from "apollo-link-context";
 
 const httpLink = new HttpLink({
-  uri: "http://localhost:5000/",
+  uri: "https://shielded-scrubland-09218-3d145831bca7.herokuapp.com/",
 });
 
 const authLink = setContext(() => {
